ea49ac550a06108b28d6108fabdd3bde75df0903
hsimao <e087754958@gmail.com>
2023-09-16 16:08:54 +0800

    feat: sync clerk user to own db user

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,32 @@
-function NewUser() {
-  return <div>Hi</div>
+import { redirect } from 'next/navigation'
+import { currentUser } from '@clerk/nextjs'
+import { prisma } from '@/utils/db'
+
+const createNewUser = async () => {
+  // NOTE: if db not yet have user need to create and redirect to jouranl page
+  const user = await currentUser()
+  if (!user) return redirect('/')
+
+  const match = await prisma.user.findUnique({
+    where: { clerkId: user.id as string },
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    })
+  }
+
+  redirect('/jouranl')
+}
+
+async function NewUser() {
+  await createNewUser()
+
+  return <div>...loading</div>
 }
 
 export default NewUser
