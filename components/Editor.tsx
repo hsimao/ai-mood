2eaa4a49d5cbfbcf344f7e1110352241fd9a1d54
hsimao <e087754958@gmail.com>
2023-09-18 20:45:41 +0800

    feat: auto save

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,13 +1,25 @@
 'use client'
 
-import { JournalEntry } from '@prisma/client'
 import { useState } from 'react'
+import { JournalEntry } from '@prisma/client'
+import { useAutosave } from 'react-autosave'
+import { updateEntry } from '@/utils/api'
 
 function Editor({ entry }: { entry: JournalEntry }) {
   const [value, setValue] = useState(entry.content)
+  const [isLoading, setIsLoading] = useState(false)
+  useAutosave({
+    data: value,
+    onSave: async (_value: string) => {
+      setIsLoading(true)
+      const updated = await updateEntry(entry.id, _value)
+      setIsLoading(false)
+    },
+  })
 
   return (
-    <div className="w-full h-full overflow-x-hidden">
+    <div className="w-full h-full overflow-hidden">
+      {isLoading && <div>...loading</div>}
       <textarea
         className="w-full h-full p-8 text-xl outline-none"
         value={value}
