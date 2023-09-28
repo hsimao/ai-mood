1344a59d856f4114515ac8971477a45362cf65d5
hsimao <e087754958@gmail.com>
2023-09-28 17:06:17 +0800

    feat: search qa function logic with vectors

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,7 +1,14 @@
 import { OpenAI } from 'langchain/llms/openai'
 import { StructuredOutputParser } from 'langchain/output_parsers'
 import { PromptTemplate } from 'langchain/prompts'
+import { Document } from 'langchain/document'
+import { loadQARefineChain } from 'langchain/chains'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
+
 import z from 'zod'
+import { JournalEntry } from '@prisma/client'
+import { relative } from 'path'
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
@@ -53,4 +60,29 @@ export const analyze = async (content: string) => {
   } catch (e) {
     console.log('error:', e)
   }
+}
+
+const qa = async (question: string, entries: JournalEntry[]) => {
+  const docs = entries.map((entry) => {
+    return new Document({
+      pageContent: entry.content,
+      metadata: { id: entry.id, createdAt: entry.createdAt },
+    })
+  })
+
+  const model = new OpenAI({
+    temperature: 0,
+    modelName: 'gpt-3.5-turbo',
+  })
+
+  const chain = loadQARefineChain(model)
+  const embeddings = new OpenAIEmbeddings()
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings)
+  const relevantDocs = await store.similaritySearch(question)
+  const res = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  })
+
+  return res.output_text
 }
